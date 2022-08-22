5f66b6ab4951fd72aea9e9c9bd0d36b9959e1ce3
Satyam Kashyap <Laughalot>
2022-08-23 02:26:58 +0530

    feat: persist state after refresh

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -2,21 +2,33 @@ import React, { useEffect, useState, useContext } from "react";
 import { useRouter } from "next/router";
 import { MyCtx } from "../contexts";
 import Loader from "../utils/Loader";
+import Fetcher from "../utils/Fetcher";
+
 type Props = {
     children: React.ReactNode;
 };
 const PrivateRoute = ({ children }: Props) => {
     const router = useRouter();
-    const { state } = useContext(MyCtx);
+    const { state, loginUser, showSnackbar } = useContext(MyCtx);
 
     useEffect(() => {
-        if (!state?.isLoggedIn && router.asPath !== "/log-username") {
-            const getUserName = localStorage.getItem("userName");
-            const getIsLoggedIn = localStorage.getItem("isLoggedIn");
-            if (!getUserName || !getIsLoggedIn) {
-                router.push("/log-username");
+        async function verfiyAuthentication() {
+            if (!state?.isLoggedIn && router.asPath !== "/log-username") {
+                const getUserName = localStorage.getItem("userName");
+                if (!getUserName) {
+                    router.push("/log-username");
+                } else {
+                    const { status } = await Fetcher(`users/${getUserName}`);
+                    if (status === 200) {
+                        await loginUser(getUserName);
+                        await router.push(`/user/${getUserName}`);
+                    } else {
+                        router.push("/log-username");
+                    }
+                }
             }
         }
+        verfiyAuthentication();
     }, []);
     if (!state?.isLoggedIn && router.asPath !== "/log-username")
         return (
